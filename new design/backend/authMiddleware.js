5a664c73b03e052dabdb8abebd9a470808adfe1e
Sh33z <mcheickali@gmail.com>
2024-11-08 14:55:20 -0500

    Create authMiddleware.js

---
+++ new design/backend/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'your_secret_key';
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ message: 'Token missing' });
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
