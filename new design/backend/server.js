5b0605a03f143b6c7d1ff5fa63e3c63eb53696e4
Sh33z <mcheickali@gmail.com>
2024-11-08 15:10:13 -0500

    Update server.js

--- new design/backend/server.js
+++ new design/backend/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const teamRoutes = require('./routes/teamRoutes');
-const evaluationRoutes = require('./routes/evaluationRoutes');
-
+const authRoutes = require('./authRoutes');
+const assessmentRoutes = require('./assessmentRoutes');
+const availabilityRoutes = require('./availabilityRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const studentDashboardRoutes = require('./studentDashboardRoutes');
+const teamRoutes = require('./teamRoutes');
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/teams', teamRoutes);
-app.use('/api/evaluations', evaluationRoutes);
+
+app.use('/api/auth', authRoutes); 
+app.use('/api/assessments', assessmentRoutes); 
+app.use('/api/availability', availabilityRoutes); 
+app.use('/api/dashboard', dashboardRoutes); 
+app.use('/api/student', studentDashboardRoutes);
+app.use('/api/teams', teamRoutes); 
 
 const PORT = 3000;
 app.listen(PORT, () => {
