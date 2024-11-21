f9d6a1acf4de2923d306763aadb166a1bde55938
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-21 01:38:45 -0500

    Create teamRoutes.test.js

---
+++ new design/backend/teamRoutes.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const express = require('express');
+const teamRoutes = require('./teamRoutes');
+
+const app = express();
+app.use(express.json());
+app.use('/teams', teamRoutes);
+
+test('GET /teams - should return all teams', async () => {
+  const res = await request(app).get('/teams');
+
+  expect(res.status).toBe(200);
+  expect(res.body).toBeInstanceOf(Array);
+});
