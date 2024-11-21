85c7b608b9505e12d4bb9fecdda67ff53f0fb20b
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-21 01:49:44 -0500

    Update jest.config.js

--- new design/backend/jest.config.js
+++ new design/backend/jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/backend/**/*.test.js'], // Match test files in any directory under backend
-  coverageDirectory: './coverage', // Save coverage reports
+  testMatch: [
+    '<rootDir>/backend/**/*.test.js', // Match backend tests
+    '<rootDir>/**/*.test.js'         // Match frontend tests
+  ],
+  coverageDirectory: './coverage',   // Save coverage reports
+  moduleDirectories: [
+    'node_modules',
+    '<rootDir>/backend',
+    '<rootDir>'
+  ]
 };
