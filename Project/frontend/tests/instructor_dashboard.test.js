adfa1490375399d23cffd004daed8d5cc20660e0
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-21 16:58:43 -0500

    Update instructor_dashboard.test.js

--- Project/frontend/tests/instructor_dashboard.test.js
+++ Project/frontend/tests/instructor_dashboard.test.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { JSDOM } = require('jsdom');
 
 test('Instructor Dashboard renders correctly', () => {
-  const html = fs.readFileSync(path.resolve(__dirname, './instructor_dashboard.html'), 'utf8');
+  const html = fs.readFileSync(path.resolve(__dirname, '../components/instructor_dashboard.html'), 'utf8');
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
