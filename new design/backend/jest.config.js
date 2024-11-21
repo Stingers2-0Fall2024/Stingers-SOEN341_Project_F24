d1a75e2b7b8b40c566dfa70af3f23ae4c1f33720
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-21 01:29:43 -0500

    Create jest.config.js

---
+++ new design/backend/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: 'node', // Use Node.js environment for backend testing
+  testMatch: ['**/*.test.js'], // Match .test.js files in any directory
+  coverageDirectory: './coverage', // Generate coverage reports in the 'coverage' folder
+};
