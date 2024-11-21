e720e3076092356882d2fb0bf7064ff82c092850
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-21 17:10:44 -0500

    Update teamRoutes.test.js

--- Project/backend/tests/teamRoutes.test.js
+++ Project/backend/tests/teamRoutes.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const express = require('express');
-const teamRoutes = require('./teamRoutes');
+const teamRoutes = require('../routes/teamRoutes');
 
 const app = express();
 app.use(express.json());
