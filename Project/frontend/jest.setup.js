1ddf5c9bedad5f69c79801a49c34886db8d9337f
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-21 16:51:40 -0500

    Update jest.setup.js

--- Project/frontend/jest.setup.js
+++ Project/frontend/jest.setup.js
@@ -1,7 +1,8 @@
+require('@testing-library/jest-dom');
+
 const { TextEncoder, TextDecoder } = require('util');
 
 // Polyfill TextEncoder and TextDecoder globally for JSDOM compatibility
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-import '@testing-library/jest-dom';
