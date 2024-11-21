5fd5b5c72899afadbc9e8d9262ba9d5fd339e4c4
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-20 22:38:05 -0500

    Update jest.config.js

--- new design/jest.config.js
+++ new design/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   setupFiles: ['./jest.setup.js'], // Path to your Jest setup file
   testEnvironment: 'jsdom',       // Required for DOM-based testing
+    transformIgnorePatterns: [
+    "/node_modules/(?!your-module-name-or-other-modules-to-transform)"
+  ]
 };
