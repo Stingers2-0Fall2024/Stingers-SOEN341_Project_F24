508facda6895f167036e9045f95b037ad9790cd8
SamuelVachon <samuelvachon2003@hotmail.com>
2024-11-24 20:04:34 -0500

    sonarqube

--- Project/backend/routes/teamRoutes.js
+++ Project/backend/routes/teamRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const db = require('../config/db');
-const authMiddleware = require('../middlewares/authMiddleware');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
