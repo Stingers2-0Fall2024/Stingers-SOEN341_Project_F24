1d8273978b5303e721fd22f1ab84f3bee04c1ef6
Reuven01 <126638627+Reuven01@users.noreply.github.com>
2024-11-26 00:09:14 -0500

    Update jest.config.js

--- Project/backend/jest.config.js
+++ Project/backend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ["./tests/db.test.js", "./tests/authMiddleware.test.js"],
+  testMatch: ["**/tests/**/*.test.js"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transform: {'^.+\\.js$': 'babel-jest'}
 };
